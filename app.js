e9bfe49f1f289dd7d6147de698ecc761c086a1a6
Thomas Song <thomas.song@efrei.net>
2024-03-21 10:46:19 +0100

    initial postgres connection

--- app.js
+++ app.js
@@ -3,39 +3,47 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var cors = require('cors')
+
 var app = express();
+const PORT = process.env.PORT || 5555;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({
+    "origin": process.env.FRONT_DOMAIN_NAME,
+    "methods": "GET,PATCH,POST,PUT,DELETE,OPTIONS",
+    "allowedHeaders": "X-Requested-With,Content-Type,Authorization",
+}))
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.listen(PORT, (error) =>{
+  if(!error)
+      console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+  else 
+      console.log("Error occurred, server can't start", error);
+  }
+);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+/* BEGIN db initialization */
+const Sequelize = require('./db.connection');
+const connection = Sequelize.connection;
+/* END db initialization */
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+try{ 
+  connection.authenticate()
+}
+catch(error){
+  console.log("Unable to connect to the database:", error);
+}
+
 
 module.exports = app;
